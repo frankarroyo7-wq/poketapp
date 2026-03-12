85c0de85b2f90b6cee243c2d419202b1256e77b8
frankarroyo7-wq <frankarroyo7@gmail.com>
2026-03-12 13:56:29 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,7 @@
-// Importamos las funciones necesarias desde la nube (SDKs)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Tu configuración (la que me acabas de pasar)
 const firebaseConfig = {
   apiKey: "AIzaSyCjNMGmRLxbRG355b1YjcNE2OF3UjLAU8k",
   authDomain: "apptcgpkt.firebaseapp.com",
@@ -13,10 +11,10 @@ const firebaseConfig = {
   appId: "1:338816496453:web:63977d47d907d5eaa0842c"
 };
 
-// Inicializamos la conexión
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportamos las herramientas para usarlas en tu archivo de lógica (app.js)
+// Exportamos las herramientas para que app.js las pueda usar
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
